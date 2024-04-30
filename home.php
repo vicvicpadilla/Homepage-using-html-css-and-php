672e163f6ead9ad4a46d757bf2c1b84fbdbaab69
vicvicpadilla <rovickplayz24@gmail.com>
2024-04-30 19:40:45 +0800

    first commit

---
+++ home.php
@@ -0,0 +1,32 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/home.css">
+    <title>Homepage</title>
+</head>
+<body>
+    <div class="nav">
+        <div class="surname">
+            <p>Padilla</p>
+        </div>
+        <div class="links">
+            <a href="logout.php"> <button class="btn">Log Out</button></a>
+        </div>
+    </div>
+    <main>
+        <div class="main-box top">
+            <div class="top">
+                <div class="box">
+                    <h3>Hello Welcome Back</h3>
+                </div>
+            </div>
+        </div>
+    </main>
+
+
+
+</body>
+</html>
